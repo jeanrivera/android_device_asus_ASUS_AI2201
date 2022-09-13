96bcb63ac16c10f05bfb19d250932051abf85a1d
Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
2022-09-13 14:59:43 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ASUS_AI2201-user
-add_lunch_combo omni_ASUS_AI2201-userdebug
-add_lunch_combo omni_ASUS_AI2201-eng
+add_lunch_combo twrp_ASUS_AI2201-eng
