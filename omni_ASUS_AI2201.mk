92f7ef9c338959095a97ac4de7af07586f9cf9fa
Carlo Dee <109793453+carlodeeCrypton@users.noreply.github.com>
2022-09-13 14:58:35 +0800

    Change some info.

--- omni_ASUS_AI2201.mk
+++ omni_ASUS_AI2201.mk
@@ -7,19 +7,21 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from ASUS_AI2201 device
-$(call inherit-product, device/asus/ASUS_AI2201/device.mk)
+$(call inherit-product, device/asus/AI2201/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product-if-exists, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ASUS_AI2201
-PRODUCT_NAME := omni_ASUS_AI2201
-PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_AI2201
-PRODUCT_MANUFACTURER := asus
+PRODUCT_DEVICE := AI2201
+PRODUCT_NAME := twrp_AI2201
+PRODUCT_BRAND := ASUS_AI2201
+PRODUCT_MODEL := ROG Phone 6
+PRODUCT_MANUFACTURER := ASUS
