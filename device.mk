1ff5a6ed7b4517b02f587e49af1bac68134de28a
Carlo Dee <carlodee.official@gmail.com>
2022-10-29 00:09:54 +0800

    ASUS_AI2201: Import from CaptainThrowback's https://github.com/CaptainThrowback/android_device_asus_sm84xx

---
+++ device.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Local path for OEM device tree
+LOCAL_PATH := device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)
+
+# Inherit from OEM SOC-common
+$(call inherit-product, $(LOCAL_PATH)/Android.mk)
+
+# A/B related packages
+PRODUCT_PACKAGES += update_engine \
+    update_engine_client \
+    update_verifier \
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+  update_engine_sideload
+
+# f2fs utilities
+PRODUCT_PACKAGES += \
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs
+
+# Userdata checkpoint
+PRODUCT_PACKAGES += \
+    checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
---
+++ twrp_AI2201.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+
+## Device identifier. This must come after all inclusions
+PRODUCT_PLATFORM := taro
+PRODUCT_RELEASE_NAME := ASUS_AI2201
+PRODUCT_DEVICE := AI2201
+PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
+PRODUCT_BRAND := Asus
+PRODUCT_MODEL := ROG Phone 6
+PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, $(DEVICE_PATH)/device-$(PRODUCT_DEVICE).mk)
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
